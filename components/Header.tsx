66ced66e9dc16e68c51e9053fdeca743927e7eb7
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-02-09 10:41:28 +0800

    feat: update header to use sgds masthead

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,37 +1,14 @@
-import styled from "styled-components";
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [`sgds-masthead`]: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
 
-import locale from "@/config/locale";
-import Merlion from "./svg/Merlion";
-
-const Wrapper = styled.header`
-  position: relative;
-  height: auto;
-  padding: 6px 0;
-  z-index: 2;
-  font-size: 14px;
-  background-color: #f0f0f0;
-`;
-
-const Container = styled.div`
-  gap: 4px;
-  color: #484848;
-  display: flex;
-  align-items: center;
-  margin-left: 30px;
-`;
-
-const Text = styled.p`
-  font-size: 14px;
-  font-weight: 400;
-  color: #484848;
-`;
-
-export default () => (
-  <Wrapper>
-    <Container>
-      <Merlion />
-
-      <Text>{locale.header}</Text>
-    </Container>
-  </Wrapper>
-);
+export default () => {
+  return <sgds-masthead />;
+};
